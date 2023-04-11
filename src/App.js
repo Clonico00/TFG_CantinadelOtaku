f2056e6a9f6970b0841351efd606047c70021e50
Javier Martinez Garcia <79504901+Clonico00@users.noreply.github.com>
2023-04-11 19:09:14 +0200

    Initialize project using Create React App and working on navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import Merchandising from "./pages/merchandising";
+import Comics from "./pages/comics";
+import Mangas from "./pages/mangas";
+import Library from "./pages/library";
+import Forum from "./pages/forum";
+import Register from "./pages/register";
+import {
+    Navbar,
+    NavbarToggler,
+    Collapse,
+    NavLink,
+    Nav,
+    NavbarBrand
+} from 'reactstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isOpen, setIsOpen] = React.useState(true);
+    return (
+        // <Router>
+        //     <Navbar />
+        //     <Routes>
+        //         <Route path="/" element={<h1>Home</h1>} />
+        //         <Route path="merchandising" component={Merchandising} />
+        //         <Route path="comics" component={Comics} />
+        //         <Route path="mangas" component={Mangas}/>
+        //         <Route path="library" component={Library} />
+        //         <Route path="forum" component={Forum} />
+        //         <Route path="register" component={Register} />
+        //     </Routes>
+        // </Router>
+        <div>
+            <BrowserRouter>
+                <h5>ReactJS Reactstrap Navbar Component</h5>
+                <Navbar color="light" light>
+                    <NavbarBrand href="/">Brand</NavbarBrand>
+                    <NavbarToggler onClick={() => {
+                        setIsOpen(!isOpen)
+                    }}/>
+                    <Collapse isOpen={isOpen} navbar>
+                        <Nav className="mr-auto" navbar>
+                            <NavLink to="/">Home</NavLink>
+                            <NavLink to="/merchandising">Merchandising</NavLink>
+
+                            <NavLink to="/comics">Comics</NavLink>
+
+                            <NavLink to="/mangas">Mangas</NavLink>
+
+                            <NavLink to="/forum">Login</NavLink>
+
+                            <NavLink to="/register">Register</NavLink>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+                <Routes>
+                    <Route path="/" element={<h1>Home</h1>}/>
+                    <Route exact path="merchandising" element={Merchandising}/>
+                    <Route exact path="comics" element={Comics}/>
+                    <Route exact path="mangas" element={Mangas}/>
+                    <Route exact path="library" element={Library}/>
+                    <Route exact path="forum" element={Forum}/>
+                    <Route exact path="register" element={Register}/>
+                </Routes>
+            </BrowserRouter>
+
+        </div>
+
+    );
 }
 
 export default App;
---
+++ src/components/NavBar/NavbarElements.js
@@ -0,0 +1,86 @@
+import { FaBars } from 'react-icons/fa';
+import { NavLink as Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const Nav = styled.nav`
+  background: #212529;
+  font-weight: 700;
+  font-size: 20px;
+  height: 85px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.2rem calc((100vw - 1000px) / 2);
+  z-index: 12;
+  
+`;
+
+export const NavLink = styled(Link)`
+  color: #ffffff;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+
+  &.active {
+    color: #212529;
+    background: white;
+  }
+  &:hover {
+    color: #212529;
+    background: white;
+  }
+`;
+
+export const Bars = styled(FaBars)`
+  display: none;
+  color: #ffffff;
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 75%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+`;
+
+export const NavMenu = styled.div`
+display: flex;
+align-items: center;
+margin-right: -24px;
+@media screen and (max-width: 768px) {
+	display: none;
+}
+`;
+
+export const NavBtn = styled.nav`
+display: flex;
+align-items: center;
+margin-right: 24px;
+
+@media screen and (max-width: 768px) {
+	display: none;
+}
+`;
+
+export const NavBtnLink = styled(Link)`
+  border-radius: 4px;
+  background: #ffffff;
+  padding: 10px 22px;
+  color: #212529;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #fff;
+    color: #808080;
+  }
+`;
