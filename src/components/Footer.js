2dda9efb17d173699130a3fcc85d4a3ebb4bf883
Javier Martinez Garcia <79504901+Clonico00@users.noreply.github.com>
2023-04-14 20:07:20 +0200

    Apply animation and change the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,89 +2,106 @@ import React from 'react';
 import {BiMap} from "react-icons/bi";
 import {AiOutlinePhone, AiOutlineMail, AiOutlineTwitter, AiOutlineInstagram} from "react-icons/ai";
 import {FaTiktok} from "react-icons/fa";
-import Wave from '../img/wave.png';
 
 const Footer = () => {
-    return <footer className="text-white py-12 w-full bg-gray-900 footer-with-svg" style={{
-        backgroundImage: `url(${Wave})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: '100%',
-        backgroundPositionY: '-350px',
-        backgroundPosition: '0',
-
-    }}>
+    return <footer className="py-12 w-full">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 sm:text-center md:text-left">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
                 <div className="grid grid-cols-1 gap-4">
                     <div className="flex items-center mb-4">
-                        <h2 className="text-lg font-semibold">Cantina del Otaku</h2>
+                        <h2 className="font-extrabold tracking-tight text-2xl whitespace-nowrap" style={{
+                            backfaceVisibility: 'hidden',
+                            color: '#1e2447'
+                        }}>Cantina del Otaku</h2>
                     </div>
                 </div>
                 <div className="grid grid-cols-1 gap-4">
-                    <h2 className="text-lg font-semibold mb-4 ">Enlaces</h2>
+                    <h2 className="font-extrabold tracking-tight text-xl whitespace-nowrap" style={{
+                        backfaceVisibility: 'hidden',
+                        color: '#1e2447'
+                    }}>Enlaces</h2>
                     <div className="flex flex-col">
                         <div className="flex items-center mb-4">
-                            <a className="text-sm hover:text-gray-400"
+                            <a className="hover:scale-105 transition-all duration-400 px-3 py-2 text-md font-extrabold hover:text-shadow-lg"
+                               style={{backfaceVisibility: 'hidden', color: '#3E62FF'}}
                                href="/">Inicio</a>
                         </div>
                         <div className="flex items-center mb-4">
-                            <a className="text-sm hover:text-gray-400"
+                            <a className="hover:scale-105 transition-all duration-400 px-3 py-2 text-md font-extrabold hover:text-shadow-lg"
+                               style={{backfaceVisibility: 'hidden', color: '#3E62FF'}}
                                href="/">Merchandising</a>
                         </div>
                         <div className="flex items-center mb-4">
-                            <a className="text-sm hover:text-gray-400"
+                            <a className="hover:scale-105 transition-all duration-400 px-3 py-2 text-md font-extrabold hover:text-shadow-lg"
+                               style={{backfaceVisibility: 'hidden', color: '#3E62FF'}}
                                href="/">Mangas</a>
                         </div>
                         <div className="flex items-center mb-4">
-                            <a className="text-sm hover:text-gray-400"
+                            <a className="hover:scale-105 transition-all duration-400 px-3 py-2 text-md font-extrabold hover:text-shadow-lg"
+                               style={{backfaceVisibility: 'hidden', color: '#3E62FF'}}
                                href="/">Comics</a>
                         </div>
                         <div className="flex items-center mb-4">
-                            <a className="text-sm hover:text-gray-400"
+                            <a className="hover:scale-105 transition-all duration-400 px-3 py-2 text-md font-extrabold hover:text-shadow-lg"
+                               style={{backfaceVisibility: 'hidden', color: '#3E62FF'}}
                                href="/">Foro</a>
                         </div>
                         <div className="flex items-center mb-4">
-                            <a className="text-sm hover:text-gray-400"
+                            <a className="hover:scale-105 transition-all duration-400 px-3 py-2 text-md font-extrabold hover:text-shadow-lg"
+                               style={{backfaceVisibility: 'hidden', color: '#3E62FF'}}
                                href="/">Libreria</a>
                         </div>
                     </div>
                 </div>
                 <div className="flex flex-col">
                     <div className="grid grid-cols-1 gap-4">
-                        <h3 className="text-lg font-semibold mb-4">Redes sociales</h3>
+                        <h2 className="font-extrabold tracking-tight text-xl whitespace-nowrap" style={{
+                            backfaceVisibility: 'hidden',
+                            color: '#1e2447'
+                        }}>Redes sociales</h2>
                         <div className="flex flex-col">
                             <div className="flex items-center mb-4">
                                 <AiOutlineInstagram className="w-6 h-6 mr-2"/>
-                                <a className="text-sm hover:text-gray-400"
+                                <a className="hover:scale-105 transition-all duration-400 px-3 py-2 text-md font-extrabold hover:text-shadow-lg"
+                                   style={{backfaceVisibility: 'hidden', color: '#3E62FF'}}
                                    href="https://www.instagram.com/">Instagram</a>
                             </div>
                             <div className="flex items-center mb-4">
                                 <AiOutlineTwitter className="w-6 h-6 mr-2"/>
-                                <a className="text-sm hover:text-gray-400"
+                                <a className="hover:scale-105 transition-all duration-400 px-3 py-2 text-md font-extrabold hover:text-shadow-lg"
+                                   style={{backfaceVisibility: 'hidden', color: '#3E62FF'}}
                                    href="https://www.twitter.com/">Twitter</a>
                             </div>
                             <div className="flex items-center mb-4">
                                 <FaTiktok className="w-6 h-6 mr-2"/>
-                                <a className="text-sm hover:text-gray-400" href="https://www.tiktok.com/">Tiktok</a>
+                                <a className="hover:scale-105 transition-all duration-400 px-3 py-2 text-md font-extrabold hover:text-shadow-lg"
+                                   style={{backfaceVisibility: 'hidden', color: '#3E62FF'}}
+                                   href="https://www.tiktok.com/">Tiktok</a>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="flex flex-col">
                     <div className="grid grid-cols-1 gap-4">
-                        <h2 className="text-lg font-semibold mb-4">Contacto</h2>
+                        <h2 className="font-extrabold tracking-tight text-xl whitespace-nowrap" style={{
+                            backfaceVisibility: 'hidden',
+                            color: '#1e2447'
+                        }}>Contacto</h2>
                         <div className="flex flex-col">
                             <div className="flex items-center mb-4">
                                 <BiMap className="w-6 h-6 mr-2"/>
-                                <p className="text-sm">Granada, España</p>
+                                <p  className="hover:scale-105 transition-all duration-400 px-3 py-2 text-md font-extrabold hover:text-shadow-lg"
+                                    style={{backfaceVisibility: 'hidden', color: '#3E62FF'}}>Granada, España</p>
                             </div>
                             <div className="flex items-center mb-4">
                                 <AiOutlinePhone className="w-6 h-6 mr-2"/>
-                                <p className="text-sm">+34 601 303 832</p>
+                                <p  className="hover:scale-105 transition-all duration-400 px-3 py-2 text-md font-extrabold hover:text-shadow-lg"
+                                    style={{backfaceVisibility: 'hidden', color: '#3E62FF'}}>+34 601 303 832</p>
                             </div>
                             <div className="flex items-center mb-4">
                                 <AiOutlineMail className="w-6 h-6 mr-2"/>
-                                <p className="text-sm">cantinadelotaku@gmail.com</p>
+                                <p  className="hover:scale-105 transition-all duration-400 px-3 py-2 text-md font-extrabold hover:text-shadow-lg"
+                                    style={{backfaceVisibility: 'hidden', color: '#3E62FF'}}>cantinadelotaku@gmail.com</p>
                             </div>
                         </div>
                     </div>
@@ -92,7 +109,8 @@ const Footer = () => {
             </div>
             <hr className="border-gray-600 my-8"/>
             <div className="text-center mt-8">
-                <p className="text-sm">&copy; 2023 Mi sitio web. Todos los derechos reservados.</p>
+                <p  className="hover:scale-105 transition-all duration-400 px-3 py-2 text-md font-extrabold"
+                    style={{backfaceVisibility: 'hidden', color: '#3E62FF'}}>&copy; 2023 Mi sitio web. Todos los derechos reservados.</p>
             </div>
         </div>
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,48 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import Logo from '../img/logo.png';
-
+import anime from 'animejs';
 function Navbar() {
     const [showMenu, setShowMenu] = useState(false);
 
-    const handleToggle = () => {
-        setShowMenu(!showMenu);
+
+
+    const animateMenu = () => {
+        anime.timeline({ easing: 'easeOutExpo' })
+            .add({
+                targets: '.menu-item',
+                opacity: [0, 1],
+                translateY: [100, 0],
+                duration: 500,
+                delay: anime.stagger(100),
+                offset: '-=300', // introduce a delay between each animation
+            });
     };
+
+    const animateMenuClose = () => {
+        anime.timeline({ easing: 'easeOutExpo' })
+            .add({
+                targets: '.menu-item',
+                opacity: [1, 0],
+                translateY: [0, 100],
+                duration: 500,
+                delay: anime.stagger(100, { start: 0, from: 'last' }),
+            });
+    };
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+        if (showMenu) {
+            animateMenuClose();
+        } else {
+            animateMenu();
+        }
+    };
+    const handleToggle = () => {
+        toggleMenu();
+    };
+
+
 
     return (
         <nav className="bg-white">
@@ -18,15 +53,12 @@ function Navbar() {
                             to="/"
                             style={{
                                 backfaceVisibility: 'hidden',
-                                color: '#3E62FF',
+                                color: '#1e2447',
                                 display: 'flex',
                                 alignItems: 'center',
-                            }}
-                        >
+                            }}>
                             <img src={Logo} alt="Logo" className="h-12 w-12 mr-2"/>
-                            <span className="font-extrabold tracking-tight text-2xl whitespace-nowrap">
-      Cantina del Otaku
-    </span>
+                            <span className="font-extrabold tracking-tight text-2xl whitespace-nowrap">Cantina del Otaku</span>
                         </Link>
                     </div>
 
@@ -37,8 +69,6 @@ function Navbar() {
                                 to="/"
                                 className="hover:scale-105 transition-all duration-400 px-3 py-2 text-md font-extrabold hover:text-shadow-lg"
                                 style={{backfaceVisibility: 'hidden', color: '#3E62FF'}}>
-
-
                                 Inicio
                             </Link>
                             <Link
@@ -77,29 +107,29 @@ function Navbar() {
                                 className="bg-transparent border-2 border-black rounded-lg w-full text-black font-md mr-3 py-1 px-2 leading-tight focus:outline-none placeholder:font-light"
                                 style={{
                                     transition: 'width 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
-                                    color: '#3E62FF',
-                                    borderColor: '#3E62FF'
+                                    color: '#1e2447',
+                                    borderColor: '#1e2447'
                                 }}
                             />
 
                             <button type="submit"
-                                    className="flex-shrink-0 font-bold border-2 border-black text-md py-1 px-4 rounded transform hover:scale-105 hover:shadow-md transition-all duration-300"
-                                    style={{backfaceVisibility: 'hidden', color: '#3E62FF', borderColor: '#3E62FF'}}>
-
+                                    className="flex-shrink-0 font-bold border-2 border-black text-md py-1 px-4 rounded-lg transform hover:scale-105 hover:shadow-md transition-all duration-300"
+                                    style={{backfaceVisibility: 'hidden', color: '#1e2447', borderColor: '#1e2447'}}>
                                 Buscar
                             </button>
-
-
                         </div>
-
                     </div>
                     <div className="-mr-2 flex md:hidden">
                         <button
                             type="button"
-                            className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white focus:outline-none"
+                            className={`inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white focus:outline-none`}
                             aria-controls="mobile-menu"
                             aria-expanded={showMenu ? 'true' : 'false'}
                             onClick={handleToggle}
+                            style={{
+                                transform: showMenu ? 'rotate(180deg)' : 'rotate(0deg)',
+                                transition: 'transform 0.2s ease',
+                            }}
                         >
                             <span className="sr-only">Open main menu</span>
                             {showMenu ? (
@@ -110,10 +140,8 @@ function Navbar() {
                                     width="1em"
                                     className="fill-current text-black"
                                 >
-                                    <path
-                                        d="M0 96c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zm0 160c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zm448 160c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32h384c17.7 0 32 14.3 32 32z"/>
+                                    <path d="M0 96c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zm0 160c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zm448 160c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32h384c17.7 0 32 14.3 32 32z" />
                                 </svg>
-
                             ) : (
                                 <svg
                                     viewBox="0 0 448 512"
@@ -122,11 +150,12 @@ function Navbar() {
                                     width="1em"
                                     className="fill-current text-black"
                                 >
-                                    <path
-                                        d="M0 96c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zm0 160c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zm448 160c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32h384c17.7 0 32 14.3 32 32z"/>
+                                    <path d="M0 96c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zm0 160c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zm448 160c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32h384c17.7 0 32 14.3 32 32z" />
                                 </svg>
                             )}
                         </button>
+
+
 
                     </div>
                 </div>
@@ -136,50 +165,50 @@ function Navbar() {
                     <Link
 
                         to="/"
-                        className="hover:scale-105 transition-all duration-400 block px-3 py-2 text-md font-extrabold hover:text-shadow-lg"
-                        style={{backfaceVisibility: 'hidden'}}>
+                        className="hover:scale-105 transition-all duration-400 block px-3 py-2 text-md font-extrabold hover:text-shadow-lg menu-item"
+                        style={{backfaceVisibility: 'hidden', color: '#3E62FF'}}>
                         Inicio
                     </Link>
                     <Link
                         to="/merchandising"
-                        className="hover:scale-105 transition-all duration-400 block px-3 py-2 text-md font-extrabold hover:text-shadow-lg"
-                        style={{backfaceVisibility: 'hidden'}}>
+                        className="hover:scale-105 transition-all duration-400 block px-3 py-2 text-md font-extrabold hover:text-shadow-lg menu-item"
+                        style={{backfaceVisibility: 'hidden', color: '#3E62FF'}}>
                         Merchandising
                     </Link>
                     <Link
                         to="/mangas"
-                        className="hover:scale-105 transition-all duration-400 block px-3 py-2 text-md font-extrabold hover:text-shadow-lg"
-                        style={{backfaceVisibility: 'hidden'}}>
+                        className="hover:scale-105 transition-all duration-400 block px-3 py-2 text-md font-extrabold hover:text-shadow-lg menu-item"
+                        style={{backfaceVisibility: 'hidden', color: '#3E62FF'}}>
                         Mangas
                     </Link>
                     <Link
                         to="/comics"
-                        className="hover:scale-105 transition-all duration-400 block px-3 py-2 text-md font-extrabold hover:text-shadow-lg"
-                        style={{backfaceVisibility: 'hidden'}}>
+                        className="hover:scale-105 transition-all duration-400 block px-3 py-2 text-md font-extrabold hover:text-shadow-lg menu-item"
+                        style={{backfaceVisibility: 'hidden', color: '#3E62FF'}}>
                         Comics
                     </Link>
                     <Link
                         to="/foro"
-                        className="hover:scale-105 transition-all duration-400 block px-3 py-2 text-md font-extrabold hover:text-shadow-lg"
-                        style={{backfaceVisibility: 'hidden'}}>
+                        className="hover:scale-105 transition-all duration-400 block px-3 py-2 text-md font-extrabold hover:text-shadow-lg menu-item"
+                        style={{backfaceVisibility: 'hidden', color: '#3E62FF'}}>
                         Foro
                     </Link>
                     <Link
                         to="/libreria"
-                        className="hover:scale-105 transition-all duration-400 block px-3 py-2 text-md font-extrabold hover:text-shadow-lg"
-                        style={{backfaceVisibility: 'hidden'}}>
+                        className="hover:scale-105 transition-all duration-400 block px-3 py-2 text-md font-extrabold hover:text-shadow-lg menu-item"
+                        style={{backfaceVisibility: 'hidden', color: '#3E62FF'}}>
                         Libreria
                     </Link>
                     <input
                         type="text"
                         placeholder="Buscar..."
-                        className="bg-transparent border-2 border-black rounded-lg w-full text-black font-bold mr-3 py-1 px-2 leading-tight focus:outline-none placeholder:font-light"
+                        className="bg-transparent border-2 border-black rounded-lg w-full text-black font-bold mr-3 py-1 px-2 leading-tight focus:outline-none placeholder:font-light menu-item"
                         style={{'transition': 'width 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms'}}
                     />
 
                     <button type="submit"
 
-                            className="flex-shrink-0 font-bold border-2 border-black text-md py-1 px-4 rounded transform hover:shadow-md transition-all duration-300 w-full mt-4"
+                            className="flex-shrink-0 font-bold border-2 border-black text-md py-1 px-4 rounded-lg transform hover:shadow-md transition-all duration-300 w-full mt-4 menu-item"
                             style={{backfaceVisibility: 'hidden'}}>
                         Buscar
                     </button>
