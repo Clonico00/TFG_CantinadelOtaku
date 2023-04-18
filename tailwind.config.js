a090336f026875b81a3e5e640501b8a365e9c975
jmargar <javier.martinez-garcia@t-systems.com>
2023-04-18 10:08:33 +0200

    Navbar responsive( no animation )

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '1100px', // Cambia este valor de 768 a 1200
+      'lg': '1024px',
+      'xl': '1280px',
+    },
     extend: {},
   },
   plugins: [
